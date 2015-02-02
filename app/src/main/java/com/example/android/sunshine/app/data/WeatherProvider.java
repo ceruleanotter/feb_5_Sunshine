40590a4040cfdf16a24c554b2d32f0cae7d188d4
dgalpin <dag@android.com>
2015-02-02 15:49:15 -0800

    Removed db.close() since it causes lots of problems.

--- app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
+++ app/src/main/java/com/example/android/sunshine/app/data/WeatherProvider.java
@@ -244,7 +244,6 @@ public class WeatherProvider extends ContentProvider {
                 throw new UnsupportedOperationException("Unknown uri: " + uri);
         }
         getContext().getContentResolver().notifyChange(uri, null);
-        db.close();
         return returnUri;
     }
 
