2bcc396d48ff8dd3d12252d9cccd562f53270e3b
Lyla <lyla@udacity.com>
2015-02-03 13:52:53 -0800

    5.03 CursorAdapter- use ForecastAdapter

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -6,6 +6,7 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 /**
@@ -58,7 +59,35 @@ public class ForecastAdapter extends CursorAdapter {
         // our view is pretty simple here --- just a text view
         // we'll keep the UI functional with a simple (and slow!) binding.
 
-        TextView tv = (TextView)view;
-        tv.setText(convertCursorRowToUXFormat(cursor));
+        // Read weather icon ID from cursor
+        int weatherId = cursor.getInt(ForecastFragment.COL_WEATHER_ID);
+        // Use placeholder image for now
+        ImageView iconView = (ImageView) view.findViewById(R.id.list_item_icon);
+        iconView.setImageResource(R.drawable.ic_launcher);
+
+        // Read date from cursor
+        long dateInMillis = cursor.getLong(ForecastFragment.COL_WEATHER_DATE);
+        // Find TextView and set formatted date on it
+        TextView dateView = (TextView) view.findViewById(R.id.list_item_date_textview);
+        dateView.setText(Utility.getFriendlyDayString(context, dateInMillis));
+
+        // Read weather forecast from cursor
+        String description = cursor.getString(ForecastFragment.COL_WEATHER_DESC);
+        // Find TextView and set weather forecast on it
+        TextView descriptionView = (TextView) view.findViewById(R.id.list_item_forecast_textview);
+        descriptionView.setText(description);
+
+        // Read user preference for metric or imperial temperature units
+        boolean isMetric = Utility.isMetric(context);
+
+        // Read high temperature from cursor
+        double high = cursor.getDouble(ForecastFragment.COL_WEATHER_MAX_TEMP);
+        TextView highView = (TextView) view.findViewById(R.id.list_item_high_textview);
+        highView.setText(Utility.formatTemperature(high, isMetric));
+
+        // Read low temperature from cursor
+        double low = cursor.getDouble(ForecastFragment.COL_WEATHER_MIN_TEMP);
+        TextView lowView = (TextView) view.findViewById(R.id.list_item_low_textview);
+        lowView.setText(Utility.formatTemperature(low, isMetric));
     }
 }
--- app/src/main/java/com/example/android/sunshine/app/Utility.java
+++ app/src/main/java/com/example/android/sunshine/app/Utility.java
@@ -20,6 +20,8 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class Utility {
@@ -49,5 +51,109 @@ public class Utility {
     static String formatDate(long dateInMillis) {
         Date date = new Date(dateInMillis);
         return DateFormat.getDateInstance().format(date);
+    }    // Format used for storing dates in the database.  ALso used for converting those strings
+    // back into date objects for comparison/processing.
+
+    public static final String DATE_FORMAT = "yyyyMMdd";
+
+    /**
+     * Helper method to convert the database representation of the date into something to display
+     * to users.  As classy and polished a user experience as "20140102" is, we can do better.
+     *
+     * @param context Context to use for resource localization
+     * @param dateInMillis The inputted date in milliseconds
+     * @return a user-friendly representation of the date.
+     */
+    public static String getFriendlyDayString(Context context, long dateInMillis) {
+        // The day string for forecast uses the following logic:
+        // For today: "Today, June 8"
+        // For tomorrow:  "Tomorrow"
+        // For the next 5 days: "Wednesday" (just the day name)
+        // For all days after that: "Mon Jun 8"
+
+        Date todayDate = new Date();
+        Date inputDate = new Date(dateInMillis);
+
+        // If the date we're building the String for is today's date, the format
+        // is "Today, June 24"
+        if (checkSameDay(inputDate, todayDate)) {
+            String today = context.getString(R.string.today);
+            int formatId = R.string.format_full_friendly_date;
+            return String.format(context.getString(
+                    formatId,
+                    today,
+                    getFormattedMonthDay(context, inputDate)));
+        } else {
+            Calendar calFuture = Calendar.getInstance();
+            Calendar calInput = Calendar.getInstance();
+            calFuture.setTime(todayDate);
+            calFuture.add(Calendar.DATE, 7);
+            calInput.setTime(inputDate);
+
+            if (calInput.compareTo(calFuture) < 0) {
+                // If the input date is less than a week in the future, just return the day name.
+                return getDayName(context, inputDate);
+            } else {
+                // Otherwise, use the form "Mon Jun 3"
+                SimpleDateFormat shortenedDateFormat = new SimpleDateFormat("EEE MMM dd");
+                return shortenedDateFormat.format(inputDate);
+            }
+        }
+    }
+
+    /**
+     * Given a day, returns just the name to use for that day.
+     * E.g "today", "tomorrow", "wednesday".
+     *
+     * @param context Context to use for resource localization
+     * @param inputDate The Date in question
+     * @return
+     */
+    public static String getDayName(Context context, Date inputDate) {
+        Date todayDate = new Date();
+        // If the date is today, return the localized version of "Today" instead of the actual
+        // day name.
+        if (checkSameDay(todayDate, inputDate)) {
+            return context.getString(R.string.today);
+        } else {
+            // If the date is set for tomorrow, the format is "Tomorrow".
+            Calendar cal = Calendar.getInstance();
+            cal.setTime(todayDate);
+            cal.add(Calendar.DATE, 1);
+            Date tomorrowDate = cal.getTime();
+            if (checkSameDay(tomorrowDate, inputDate)) {
+                return context.getString(R.string.tomorrow);
+            } else {
+                // Otherwise, the format is just the day of the week (e.g "Wednesday".
+                SimpleDateFormat dayFormat = new SimpleDateFormat("EEEE");
+                return dayFormat.format(inputDate);
+            }
+        }
+    }
+
+    /**
+     * Converts db date format to the format "Month day", e.g "June 24".
+     * @param context Context to use for resource localization
+     * @param inputDate The Date in question
+     * @return The day in the form of a string formatted "December 6"
+     */
+    public static String getFormattedMonthDay(Context context, Date inputDate) {
+        SimpleDateFormat monthDayFormat = new SimpleDateFormat("MMMM dd");
+        String monthDayString = monthDayFormat.format(inputDate);
+        return monthDayString;
+    }
+
+    /**
+     * Compares date equality based on day by converting to a string format that only has
+     * granularity down to the day and then compares the two.
+     *
+     * @param dateOne The first date to compare
+     * @param dateTwo The second date to compare
+     * @return A boolean as to whether the two dates were on the same day.
+     */
+    public static boolean checkSameDay(Date dateOne, Date dateTwo) {
+        SimpleDateFormat compareFormat = new SimpleDateFormat(DATE_FORMAT);
+        return compareFormat.format(dateOne).
+                equals(compareFormat.format(dateTwo));
     }
 }
